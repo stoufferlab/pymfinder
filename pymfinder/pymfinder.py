0054d5cba2ff1f392a4e2a1b501fa48a9aee8117
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2013-02-01 16:20:40 +1300

    finally added the revamped UNIPARTITE MOTIF ROLES code

--- pymfinder/pymfinder.py
+++ pymfinder/pymfinder.py
@@ -24,6 +24,60 @@ STOUFFER_MOTIF_IDS = {12:  'S1',
                       78:  'D8',
                       }
 
+#key = size:(motif_id, (indegree,outdegree))
+UNIPARTITE_ROLES = {2:[(2, [(0, 1),
+                            (1, 0),
+                            ]),
+                       (6, [(1, 1),
+                            ]),
+                       ],
+
+                    3:[(12,  [(0, 1),
+                              (1, 1),
+                              (1, 0),
+                              ]),
+                       (38,  [(0, 2),
+                              (1, 1),
+                              (2, 0),
+                              ]),
+                       (98,  [(1, 1),
+                              ]),
+                       (36,  [(0, 1),
+                              (2, 0),
+                              ]),
+                       (6,   [(0, 2),
+                              (1, 0),
+                              ]),
+                       (46,  [(1, 2),
+                              (2, 0),
+                              ]),
+                       (108, [(0, 2),
+                              (2, 1),
+                              ]),
+                       (14,  [(1, 2),
+                              (1, 1),
+                              (1, 0),
+                              ]),
+                       (74,  [(0, 1),
+                              (1, 1),
+                              (2, 1),
+                              ]),
+                       (102, [(1, 1),
+                              (2, 1),
+                              (1, 2),
+                              ]),
+                       (238, [(2, 2),
+                              ]),
+                       (110, [(1, 2),
+                              (2, 1),
+                              (2, 2),
+                              ]),
+                       (78,  [(1, 1),
+                              (2, 2),
+                              ]),
+                       ],
+                    }
+
 ##############################################################
 ##############################################################
 # GENERAL UTILITIES
@@ -107,6 +161,24 @@ def mfinder_network_setup(network):
     else:
         sys.stderr.write("Uncle Sam frowns upon tax cheats.\n")
         sys.exit()
+
+# turn an mfinder-style network into a human-intelligible one
+def human_network_setup(network):
+    if type(network) == type("hello world"):
+        # DEBUG: if we want to use a filename we need to run a check here to make sure that the node labels are integers and that there are weights
+        # web.Filename = network
+        network = read_links(network)
+        return relabel_nodes(network)
+    elif type(network) == type([1,2,3]):
+        return relabel_nodes(network)
+    else:
+        sys.stderr.write("Whatcha talkin' 'bout Willis?\n")
+        sys.exit()
+
+# if we've relabeled the nodes, make sure the output corresponds to the input labels
+def decode_stats(stats,node_dictionary):
+    reverse_dictionary = dict([(j,i) for i,j in node_dictionary.items()])
+    return dict([(reverse_dictionary[i],j) for i,j in stats.items()])
 
 ##############################################################
 ##############################################################
@@ -385,10 +457,6 @@ def participation_stats(mfinderi,stoufferIDs):
     else:
         return participation    
 
-def decode_participation(participation_stats,node_dictionary):
-    reverse_dictionary = dict([(j,i) for i,j in node_dictionary.items()])
-    return dict([(reverse_dictionary[i],j) for i,j in participation_stats.items()])
-
 def motif_participation(network,
                         motifsize = 3,
                         maxmemberslistsz = 1000,
@@ -421,7 +489,7 @@ def motif_participation(network,
     p_stats = participation_stats(web,stoufferIDs)
 
     try:
-        return decode_participation(p_stats,node_dict)
+        return decode_stats(p_stats,node_dict)
     except UnboundLocalError:
         return p_stats
 
@@ -437,6 +505,161 @@ def print_participation(participation_stats,outFile=None,sep=" ",header=False):
 
     for n in sorted(participation_stats.keys()):
         output = sep.join([str(n)] + list(map(str,[j for i,j in sorted(participation_stats[n].items())])))
+        fstream.write(output + '\n')
+
+    if outFile:
+        fstream.close()
+
+    return
+
+##############################################################
+##############################################################
+# MOTIF ROLES CODE
+##############################################################
+##############################################################
+
+def role_stats(mfinderi,network,stoufferIDs):
+    results = mfinder.motif_participation(mfinderi)
+
+    maxed_out_member_list = False
+    max_count = 0
+    while True:
+        maxed_out_member_list = False
+
+        r_l = results.l
+        while (r_l != None):
+            motif = mfinder.get_motif(r_l.p)
+
+            if(int(motif.count) != motif.all_members.size):
+                maxed_out_member_list = True
+                max_count = max(max_count, int(motif.count))
+
+            r_l = r_l.next
+
+        if maxed_out_member_list:
+            sys.stderr.write("upping the anty bitches!\n")
+            mfinderi.MaxMembersListSz = max_count + 1
+            results = mfinder.motif_participation(mfinderi)
+
+        else:
+            break
+
+    _network = [(i,j) for i,j,k in network]
+
+    possible_roles = []
+    for motif,roles in UNIPARTITE_ROLES[mfinderi.MotifSize]:
+        possible_roles += [tuple([motif] + list(role)) for role in roles]
+
+    roles = {}
+    r_l = results.l
+    members = mfinder.intArray(mfinderi.MotifSize)
+    while (r_l != None):
+        motif = mfinder.get_motif(r_l.p)
+        id = int(motif.id)
+        mindex = [i for i,j in UNIPARTITE_ROLES[mfinderi.MotifSize]].index(id)
+        
+        am_l = motif.all_members.l
+        while (am_l != None):
+            mfinder.get_motif_members(am_l.p, members, mfinderi.MotifSize)
+            py_members = [int(members[i]) for i in xrange(mfinderi.MotifSize)]
+
+            for m in py_members:
+                if m not in roles:
+                    roles[m] = {}
+
+                indegree  = sum([(othernode,m) in _network for othernode in py_members if othernode != m])
+                outdegree = sum([(m,othernode) in _network for othernode in py_members if othernode != m])
+
+                key = (id, indegree, outdegree)
+
+                if key not in possible_roles:
+                    if indegree > 0:
+                        connected_to = [othernode for othernode in py_members if othernode != m and (othernode,m) in _network]
+                        outdegrees = [sum([(i,j) in _network for j in py_members if j != i]) for i in connected_to]
+                        outdegrees.sort()
+                        key = tuple(list(key) + [tuple(outdegrees)])
+                    else:
+                        connected_to = [othernode for othernode in py_members if othernode != m and (m,othernode) in _network]
+                        indegrees = [sum([(j,i) in _network for j in py_members if j != i]) for i in connected_to]
+                        indegrees.sort()
+                        key = tuple(list(key) + [tuple(indegrees)])
+
+                if key not in possible_roles:
+                    print >> sys.stderr, key
+                    print >> sys.stderr, "Apparently there is a role you aren't accounting for in roles.py."
+                    sys.exit()
+
+                try:
+                    roles[m][key] += 1
+                except KeyError:
+                    roles[m][key] = 1
+
+            am_l = am_l.next
+
+        r_l = r_l.next
+
+    for n in roles:
+        for r in possible_roles:
+            try:
+                x = roles[n][r]
+            except KeyError:
+                roles[n][r] = 0
+
+    return roles
+
+def motif_roles(network,
+                motifsize = 3,
+                maxmemberslistsz = 1000,
+                randomize = False,
+                usemetropolis = False,
+                stoufferIDs = False,
+                ):
+
+    # initialize the heinous input struct
+    web = mfinder.mfinder_input()
+
+    # setup the network info
+    web.Edges, web.NumEdges, node_dict = mfinder_network_setup(network)
+
+    # parameterize the analysis
+    web.MotifSize = motifsize
+    web.MaxMembersListSz = maxmemberslistsz
+
+    # do we want to randomize the network first?
+    if not randomize:
+        web.Randomize = 0
+    else:
+        web.Randomize = 1
+        # if so, should we use the metropolis algorithm?
+        if not usemetropolis:
+            web.UseMetropolis = 0
+        else:
+            web.UseMetropolis = 1
+    
+    network, node_dict = human_network_setup(network)    
+    r_stats = role_stats(web,network,stoufferIDs)
+
+    try:
+        return decode_stats(r_stats,node_dict)
+    except UnboundLocalError:
+        return r_stats
+
+def print_roles(role_stats,motifsize,outFile=None,sep=" ",header=False):
+    if outFile:
+        fstream = open(outFile,'w')
+    else:
+        fstream = sys.stdout
+
+    roles = []
+    for motif,rs in UNIPARTITE_ROLES[motifsize]:
+        roles += [tuple([motif] + list(r)) for r in rs]
+
+    if header:
+        output = sep.join(["node"]+list(map(str,range(1,len(roles)+1))))
+        fstream.write(output + '\n')
+
+    for n in sorted(role_stats.keys()):
+        output = sep.join([str(n)] + list(map(str,[role_stats[n][role] for role in roles])))
         fstream.write(output + '\n')
 
     if outFile:
